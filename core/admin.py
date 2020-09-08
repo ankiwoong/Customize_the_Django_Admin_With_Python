f42cd267466b674516542858ec8e6241c3847aa5
Ankiwoong <ankiwoong@gmail.com>
2020-09-08 15:27:56 +0900

    pages you use to add or change object pages

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,21 @@ from django.urls import reverse
 from django.utils.http import urlencode
 from django.db.models import Avg
 from django.utils.html import format_html
+from django import forms
 
 from core.models import Person, Course, Grade
+
+
+class PersonAdminForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = "__all__"
+
+    def clean_first_name(self):
+        if self.cleaned_data["first_name"] == "Spike":
+            raise forms.ValidationError("No Vampires")
+
+        return self.cleaned_data["first_name"]
 
 
 @admin.register(Person)
@@ -12,6 +25,8 @@ class PersonAdmin(admin.ModelAdmin):
     search_fields = ("last_name__startswith",)
     list_display = ("last_name", "first_name", "show_average")
     fields = ("first_name", "last_name", "courses")
+
+    form = PersonAdminForm
 
     def show_average(self, obj):
         result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
