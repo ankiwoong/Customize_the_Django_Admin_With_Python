e1358f5e0c77e323a3502668736883b05e66d868
Ankiwoong <ankiwoong@gmail.com>
2020-09-08 12:52:43 +0900

    you add a column to the admin that displays each student’s grade average

--- core/admin.py
+++ core/admin.py
@@ -5,10 +5,13 @@ from core.models import Person, Course, Grade
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ("last_name", "first_name")
+    list_display = ("last_name", "first_name", "show_average")
 
-    class Meta:
-        ordering = ("last_name", "first_name")
+    def show_average(self, obj):
+        from django.db.models import Avg
+
+        result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
+        return result["grade__avg"]
 
 
 @admin.register(Course)
